2d6f7684868190a6db46d2dd4039e48bd62f84e4
jeansuh42 <jeansuh42@gmail.com>
2024-09-02 23:35:23 +0900

    refactor: 요구사항 변경에 따른 패키지 이름 변경
    * paystate -> paygate

--- incer-paygate/src/main/java/incerpay/paygate/IncerPaymentApplication.java
+++ incer-paygate/src/main/java/incerpay/paygate/IncerPaymentApplication.java
@@ -1,4 +1,4 @@
-package incerpay.incerpayment;
+package incerpay.paygate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- incer-paygate/src/test/java/incerpay/paygate/IncerPaymentApplicationTests.java
+++ incer-paygate/src/test/java/incerpay/paygate/IncerPaymentApplicationTests.java
@@ -1,10 +1,8 @@
-package incerpay.incerpayment;
+package incerpay.paygate;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 
-@Import(TestcontainersConfiguration.class)
 @SpringBootTest
 class IncerPaymentApplicationTests {
 
